e1c520d4293394756dd210ebe6cfdfd4cb4d67b1
stevensotelo <stevenbetancurt@hotmail.com>
2016-09-28 10:41:29 -0500

    I was working on the map and fixed that it shows the full screen map

--- web_app/app/scripts/controllers/map.js
+++ web_app/app/scripts/controllers/map.js
@@ -9,9 +9,11 @@
  */
 angular.module('genebanksDistributionApp')
   .controller('MapCtrl', function ($scope, GenebankFactory) {
+    $(".angular-google-map-container").css('height',$(document).height());
+
     $scope.map = {
-      center: { latitude: 0, longitude: 0 },
-      zoom: 2,
+      center: { latitude: 10, longitude: 10 },
+      zoom: 1,
       showData: true,
       dataLayerCallback: function(layer) {
         //set the data layer's backend data
